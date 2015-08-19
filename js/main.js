7e232e50c699d7b90c5efaa54f1fe149d0e3ead1
myeongwon <lighitng_one@hanmail.net>
2015-08-19 18:19:42 +0900

    웹접근성 준수 스킵네비게이션.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function(global, $, undefined){
+	'use strict';
+
+	var $skip = $('#skip-navigation');
+
+	// var $skip_links = $skip.find('a').wrapAll('<ul>').wrap('<li>'); 
+	// console.log($skip_links);
+	var $skip_links = $skip.addClass('skip-content').find('a').addClass('a11y-hidden focusable');
+
+	$skip_links.on('click', function(e){
+		e.preventDefault();
+ 
+		// 포커스가 이동해야 할 목적지????
+		// e.taget <a href="#header">
+		var $target = $(e.target.getAttribute('href'));
+			$target.attr('tabindex', -1).focus();
+	});
+
+
+})(window, window.jQuery);
--- js/skip.js
+++ js/skip.js
@@ -9,6 +9,35 @@
 	// 스킵 내비게이션 요소의 링크에 클래스 설정
 	var $skip_links = $skip.find('a').addClass('a11y-hidden focusable');
 
+
+	// 시작 : options.cosutomClass 값이 거짓(false)이면 아래 코드 실행 
+	var a11y_hidden_class = {
+		'overflow' : 'hidden',
+		'position' : 'absolute',
+		'clip' : 'rect(0 0 0 0)'
+		'clip': 'rect(0, 0, 0, 0)' 
+		'width': '1px',
+		'height': '1px',
+		'margin': '-1px',
+		'padding': 0,
+		'border': 0;
+	}
+
+	var a11y_hidden_class_focusable = {
+		'overflow' : 'visible',
+		'position' : 'static',
+		'clip' : 'auto',
+		'width': 'auto',
+		'height': 'auto',
+		'margin': 0;
+	}
+	option.customClass || $skip_links.css(a11y_hidden_class)
+	.on('focus', function(ev){
+		$(this).css(a11y_hidden_class_focusable);
+	}).on('blur', function(ev){
+		$(this).css(a11y_hidden_class);
+	});
+
 	// 스킵 내비게이션 요소의 링크 클릭 이벤트 설정
 	// 이벤트의 위임.
 	$skip.on('click', 'a', function(ev){
@@ -22,5 +51,7 @@
 		$(target).attr('tabindex', -1).focus();
 	});
 	
+
+
 
 })(window, window.jQuery);
